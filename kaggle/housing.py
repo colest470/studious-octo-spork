804902c2265c408a4529115833335e4c7ee0953f
Mark Tom <markbironga@gmail.com>
2026-05-29 04:28:20 -0500

    best tree size

--- kaggle/housing.py
+++ kaggle/housing.py
@@ -13,9 +13,37 @@ X = housing_data[[['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', '
 
 y = housing_data.Price
 
-model = DecisionTreeRegressor(random_state=1)
+def get_mae(max_leaf_nodes, train_X, X_test, train_y, y_test):
+    model = DecisionTreeRegressor(random_state=0, max_leaf_nodes=max_leaf_nodes)
+    model.fit(train_X, train_y)
+    predicted = model.predict(X_test)
+    mae = mean_absolute_error(y_test, predicted)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, shuffle=True)
+    return mae
+
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+
+mae_arr = []
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, random_state=1)
+
+for max_leaf_nodes in candidate_max_leaf_nodes:
+    mae_arr.append(get_mae(max_leaf_nodes, X_train, X_test, y_train, y_test))
+    
+position = 0
+least = 0
+for index, mae in enumerate(mae_arr):
+    if index == 0:
+        least = mae_arr[index]
+
+    if least > mae:
+        least = mae
+        position = index
+        
+
+best_tree_size = candidate_max_leaf_nodes[position]
+
+model = DecisionTreeRegressor(random_state=0, max_leaf_nodes=best_tree_size)
 
 model.fit(X_train, y_train)
 
